89eabee122a57599006f1450758f15b8bfa50f6c
mcoluzzi <mcoluzzi@visitops.com>
2018-02-10 16:09:53 -0500

    Added functionality to Create Budget! button to calculate budget and categories from user input

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-const myBudget = new Budget(2000, .25, .25, .25, .25);
+var totBudget;
+var entertainmentPer;
+var foodPer;
+var clothingPer;
+var billsPer;
+var myBudget;
+
+$(() => {
+  $("#create-budget-button").click(function() {
+    totBudget = parseFloat($("#total-budget-input").val());
+    console.log(totBudget);
+    entertainmentPer = parseFloat($("#entertainment-budget-input").val()) /100;
+    console.log(entertainmentPer);
+    foodPer = parseFloat($("#food-budget-input").val()) / 100;
+    console.log(foodPer);
+    clothingPer = parseFloat($("#clothing-budget-input").val()) / 100;
+    billsPer = parseFloat($("#bills-budget-input").val()) / 100;
+    myBudget = new Budget(totBudget, entertainmentPer, foodPer, clothingPer, billsPer);
+  });
+});
+
+// var totBudget = $("#total-budget-input").value
+// var entertainmentPer = $("#entertainment-budget-input").value /100;
+// var foodPer =$("#food-budget-input").value / 100;
+// var clothingPer = $("#clothing-budget-input").value / 100;
+// var billsPer = $("#bills-budget-input").value / 100;
+
+
 
 function addButton(category, amount, description) {
-  // if (category === "Entertainment") {
-  //   entertainment.addExpense(description, amount);
-  // } else if (category === "Food") {
-  //   food.addExpense(description, amount);
-  // } else if (category === "Clothing") {
-  //   clothing.addExpense(description, amount);
-  // } else if (category === "Bills") {
-  //   bills.addExpense(description, amount);
-  // } else {
-  //   console.log("this is not a valid expense");
-  // }
   myBudget[category].addExpense(amount, description);
 }
